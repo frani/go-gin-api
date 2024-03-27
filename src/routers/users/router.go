1bcfd371c059e1fa5d2d58f5801423e3d41b1eb4
frani <franinnocenti@gmail.com>
2024-03-27 16:01:49 -0300

    chore: add authotize middleware to users endpoints

--- src/routers/users/router.go
+++ src/routers/users/router.go
@@ -1,12 +1,15 @@
 package users
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/frani/go-gin-api/src/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
-func InitRouter(r *gin.RouterGroup) *gin.RouterGroup {
+func InitRouter(r *gin.RouterGroup) {
+	r.Use(middlewares.Authorize("admin"))
 	r.GET("/", ListUsers)
 	r.POST("/", PostUser)
 	r.GET("/:id", GetUser)
 	r.PATCH("/:id", PatchUser)
 	r.DELETE("/:id", DeleteUser)
-	return r
 }
